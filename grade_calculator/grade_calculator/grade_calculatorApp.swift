f50b6ef521ae2944a36ce9157e65c03f3330fb77
zu-greta <gretarm.zu@gmail.com>
2023-12-29 17:13:14 -0500

    python_code

---
+++ grade_calculator/grade_calculator/grade_calculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  grade_calculatorApp.swift
+//  grade_calculator
+//
+//  Created by Greta Zu on 2023-12-19.
+//
+
+import SwiftUI
+
+@main
+struct grade_calculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
